a99fcf8d3ab98ce21b51d1c6230999bbe512562d
masha-krotkykh <31903446+masha-krotkykh@users.noreply.github.com>
2019-02-25 21:39:36 -0500

    Started assignment 5
    Works:
    - Give Up
    - Say Again
    - Score
    Doesn't work:
    - "I think..."

--- Assignment_05/js/script.js
+++ Assignment_05/js/script.js
@@ -145,9 +145,18 @@ let animals = [
       "yak",
       "zebra"
 ];
+
+// An array of all possible answers
 let answers = [];
+// For now we are offering 5 options to choose from
 const NUM_OPTIONS = 5;
+// There is only one correct answer in each round
 let correctAnimal;
+
+// Current score
+let score = 0;
+
+// Random parameters for the voice
 let options = {
   rate: Math.random(),
   pitch: Math.random()
@@ -156,6 +165,23 @@ let options = {
 $(document).ready(function() {
   $("#clickable").on('click', function() {
     $(this).remove();
+    $('#score').text('Your current score: ' + score);
+
+    if (annyang) {
+      // Let's define our first command. First the text we expect, and then the function it should call
+      var commands = {
+        'i give up': giveUp,
+        'say it again': speakAnimal,
+        // 'i think it is': tryOption
+      };
+
+      // Add our commands to annyang
+      annyang.addCommands(commands);
+
+      // Start listening. You can call this here, or attach this call to an event, button, etc.
+      annyang.start();
+    }
+
     startGame();
   });
 });
@@ -172,14 +198,23 @@ function addButton(label) {
   $guess.on('click', function(){
     if($(this).text() === correctAnimal) {
       console.log("correct!");
+      // remove all buttons
       $('.guess').remove();
+      // start new round
       setTimeout(newRound, 1000);
+      score += 1;
     }
     else {
       console.log("wrong!");
+      // shake the wrong button and repeat the correct animal
+      $(this).effect('shake');
       speakAnimal(correctAnimal);
+      // and reset the score
+      score = 0;
     }
+    $('#score').text('Your current score: ' + score);
   });
+
   $('body').append($guess);
 }
 
@@ -190,9 +225,10 @@ function newRound() {
     addButton(animal);
     answers.push(animal);
   }
+  // Choosinng a random animal from an array to be the correct animal
   correctAnimal = answers[Math.floor(Math.random() * answers.length)];
-
   speakAnimal(correctAnimal);
+
 }
 
 function speakAnimal() {
@@ -201,5 +237,23 @@ function speakAnimal() {
     rate: Math.random(),
     pitch: Math.random()
   };
+
   responsiveVoice.speak(reverseAnimal, "UK English Male", options);
+}
+
+function giveUp() {
+  $('.guess').each(function () {
+    if ($(this).text() === correctAnimal){
+      $(this).effect('shake');
+    }
+  });
+
+  setTimeout(function() {
+  // Remove all the buttons
+    $('.guess').remove();
+  }, 1000);
+
+  score = 0;
+  // Start a new round
+  setTimeout(newRound,1000);
 }
