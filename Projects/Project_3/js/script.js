a617f815de60606ae01cdce55f28e0ed9e16568c
masha-krotkykh <31903446+masha-krotkykh@users.noreply.github.com>
2019-04-01 22:25:37 -0400

    Draggables
    Made images draggable and resizable om mousedown
    - Left mouse key to make image smaller
    - Right mouse key tomake image bigger

--- Projects/Project_3/js/script.js
+++ Projects/Project_3/js/script.js
@@ -15,6 +15,8 @@ let images = [];
 for(let i = 0; i < imageTotal; i ++) {
   images.push(i+'.png');
 }
+let maxWidth = 500;
+let minWidth = 50;
 
 let imageQty = 10;
 console.log(images);
@@ -24,7 +26,28 @@ $(document).ready(function() {
   for (let i = 0; i < imageQty; i ++) {
     let randomTop = Math.floor(Math.random() * $('#collage').height());
     let randomLeft = Math.floor(Math.random() * $('#collage').width());
-    let randomWidth = Math.floor(Math.random() * 500);
+    let randomWidth = Math.floor(Math.random() * 350) + 50;
     $('<img src="assets/images/' + images[Math.floor(Math.random() * images.length)] + '" style="top:'+randomLeft+'px; left:'+randomTop+'px; width:'+randomWidth+'px;">').appendTo('#collage');
   }
+  $('img').draggable();
+
+  $('img').mousedown(function(event) {
+    switch(event.which) {
+      case 1:
+      if($(this).width() > 50) {
+        $(this).css ({
+          'width' : $(this).width() / 1.1
+        })
+      }
+      break;
+
+      case 3:
+      if($(this).width() < 500) {
+        $(this).css ({
+          'width' : $(this).width() * 1.1
+        })
+      }
+      break;
+    }
+  })
 })
