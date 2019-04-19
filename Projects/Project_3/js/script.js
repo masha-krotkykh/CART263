a4299fa0137ebdfc953c36f35f55f40a8ce2e20b
masha-krotkykh <31903446+masha-krotkykh@users.noreply.github.com>
2019-04-19 19:40:10 -0400

    Added RiMarkov functionality
    there are two options now:
    - Generate Markov chain by n-grams for more nonsensical Hugo Ball's "sound poetry" style result
    - Or generate RiMarkov output by words for more Tristan Tzara's cut-up poems styled result
    - Also almost fixed rotatation issue where both clockwise nd anti-clockwise rotations strart from the current position and not from zero

--- Projects/Project_3/js/script.js
+++ Projects/Project_3/js/script.js
@@ -16,7 +16,7 @@ The Project Gutenberg Literary Archive Foundation -- http://gutenberg.net/licens
 ******************/
 
 // --------------------------------IMAGES----------------------------- //
-let imageTotal = 63; // total number of images in the mmage folder
+let imageTotal = 85; // total number of images in the image folder
 let images = []; // empty image array
 
 // populating an arry with images
@@ -26,18 +26,19 @@ for(let i = 0; i < imageTotal; i ++) {
 
 let maxWidth = 500; // max. width of an image
 let minWidth = 50; // min. width of an image
-
 let imageQty = 10; // max. number of images to be displayed at the beginning
 
-let posRotation = 10; // rotation increment of 10deg.
-let negRotation = 10; // rotation increment of -10deg.
+let rotationAngle = 10; // increment for rotation of images
+let currentRotation = 0; // setting current rotation of images
+
 let scaleX = -1; // value for reflecting images along X-axis
 
 // --------------------------------POETRY------------------------------ //
-var order = 4; // number of letters in the n-gram
+var order = 3; // number of letters in the n-gram
 var ngrams = {}; // JS object to store info on all possible outcomes for given n-gram
 var beginnings = [];
 var verseLength = 30;
+var result;
 var txt = $.ajax({
   url: "assets/txt/comMan.txt",
   async: false
@@ -54,9 +55,9 @@ $(document).ready(function() {
     addImage();
   }
 
-  // $( ".resizable" ).resizable({
-  //   alsoResize: "#mirror"
-  // });
+  $( ".resizable" ).resizable({
+    alsoResize: "#mirror"
+  });
 
   $('#bin').droppable({
     drop: function(event, ui) {
@@ -93,17 +94,17 @@ $(document).ready(function() {
       break;
 
       case 115: // A key rotates image anti-clockwise by 10 deg.
+      currentRotation += rotationAngle;
       $('.active').css({
-        "transform": 'rotate(' + posRotation + 'deg)'
+        "transform": 'rotate(' + currentRotation + 'deg)'
       });
-      posRotation += 10;
       break;
 
       case 97: // S key rotates image clockwise by 10 deg.
+      currentRotation -= rotationAngle;
       $('.active').css({
-        "transform": 'rotate(' + negRotation + 'deg)'
+        "transform": 'rotate(' + currentRotation + 'deg)'
       });
-      negRotation -= 10;
       break;
 
       case 32: // Spacebar flips image on X-axis
@@ -121,9 +122,9 @@ $(document).ready(function() {
 //z = 122; x = 120;
   // ------------------------POETRY ON READY----------------------------- //
 
-  var riTxt = new RiString(txt); // Split the string of text into individual words
-  sentences = RiTa.splitSentences(txt);
-  words = riTxt.words();
+  //var riTxt = new RiString(txt); // Split the string of text into individual words
+  sentences = RiTa.splitSentences(txt); // Splitting the mass of text into individual sentences
+  //words = riTxt.words();
   // //
   // var randomWord = words[Math.floor(Math.random()*words.length)];
 
@@ -147,11 +148,17 @@ $(document).ready(function() {
     }
   }
 
-  // Create a generate button and append it to the page
-  var genButton = document.createElement("BUTTON");
-  genButton.innerHTML = "Do DaDa";
-  document.getElementById("poem").appendChild(genButton);
-  genButton.addEventListener("click", markov); // run markov function on button press
+  // Create a generate button for n-grams as letters and append it to the page
+  var sylButton = document.createElement("BUTTON");
+  sylButton.innerHTML = "Do DaDa";
+  document.getElementById("poem").appendChild(sylButton);
+  sylButton.addEventListener("click", markov); // run markov function on button press
+
+  // Create a generate button for n-grams as words and append it to the page
+  var wordButton = document.createElement("BUTTON");
+  wordButton.innerHTML = "Do Da";
+  document.getElementById("poem").appendChild(wordButton);
+  wordButton.addEventListener("click", riMarkov); // run markov function on button press
 
   // Create a clear button and append it to the page
   var clearButton = document.createElement("BUTTON");
@@ -163,20 +170,26 @@ $(document).ready(function() {
 // -----------------------POETRY FUNCTIONS----------------------------- //
 function markov() {
   var currentGram = beginnings[Math.floor(Math.random() * beginnings.length)]; // the n-gram that we start the poem with
-  var result = currentGram;
+  result = currentGram;
 
   for (var i = 0; i < verseLength; i++) {
     var possibilities = ngrams[currentGram]; // for the current n-gram check the array of possibilities for the next char
-
     var next = possibilities[Math.floor(Math.random() * possibilities.length)]; // pick a random option from this array
     result += next;
     currentGram = result.substring(result.length - order, result.length);
-
-    console.log(currentGram);
+    //console.log(currentGram);
   }
-  var paragraph = document.createElement("P"); // Create a p element in the DOM
-  paragraph.innerText = result;
-  document.getElementById("result").appendChild(paragraph); // Append p to the "poem" div
+  pushResult();
+  console.log(result);
+}
+
+function riMarkov() {
+  var riMarkovTxt = new RiMarkov(2);
+  riMarkovTxt.loadText(txt);
+  result = riMarkovTxt.generateSentences(1);
+
+  pushResult();
+  console.log(result);
 }
 
 function clear() {
@@ -185,20 +198,29 @@ function clear() {
   //}
 }
 
+function pushResult() {
+  var paragraph = document.createElement("P"); // Create a p element in the DOM
+  paragraph.innerText = result;
+  document.getElementById("result").appendChild(paragraph); // Append p to the "poem" div
+}
 
 // -----------------------IMAGES FUNCTIONS----------------------------- //
 function addImage() {
   let randomTop = Math.floor(Math.random() * 350);
   let randomLeft = Math.floor(Math.random() * 850);
-  let randomWidth = Math.floor(Math.random() * 250) + 50;
+  let randomWidth = Math.floor(Math.random() * 200) + 50;
   $('<img src="assets/images/' + images[Math.floor(Math.random() * images.length)] + '" style="top:'+randomTop+'px; left:'+randomLeft+'px; width:'+randomWidth+'px; max-width: 100%;">').appendTo('#collage');
-  $('img').draggable({
-    stack: ".images"
+
+  $('img').addClass("images"); // adding a class to all <img> DOM elements
+  $('#pointer').removeClass("images");
+
+  $('.images').draggable({
+    stack: ".images",
+    scroll: false
   }); // making images draggable to move them around and to set currently dragged image to front
 
-  $('img').addClass("images");
   // When the mouse is over an image it becomes active and allows for transfomations
-  $('img').mouseover(function() {
+  $('.images').mouseover(function() {
     $(this).addClass("active");
   });
 
