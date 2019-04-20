0e4698e40fcb543282fb2ea94ea773b263296e81
masha-krotkykh <31903446+masha-krotkykh@users.noreply.github.com>
2019-04-19 20:14:03 -0400

    CSS

--- Projects/Project_3/js/script.js
+++ Projects/Project_3/js/script.js
@@ -5,12 +5,14 @@
 Project_3
 Masha Krotkykh
 Credits:
-Daniel Shiffman -- tutorial on Markov Chains:
+Daniel Shiffman -- tutorials on Markov Chains:
 https://www.youtube.com/watch?v=6z7GQewK-Ks
 https://www.youtube.com/watch?v=9r8CmofnbAQ
 and RiTa:
 https://www.youtube.com/watch?v=VaAoIaZ3YKs
-D. Howe Github Repo.: https://github.com/dhowe/RiTaJS
+RiTa library reference page: https://rednoise.org/rita/reference/index.php
+D. Shiffman's A2Z Github Repo.: https://github.com/shiffman/A2Z-F16
+D. Howe's Github Repo.: https://github.com/dhowe/RiTaJS
 The Project Gutenberg Literary Archive Foundation -- http://gutenberg.net/license
 
 ******************/
@@ -36,44 +38,46 @@ let scaleX = -1; // value for reflecting images along X-axis
 // --------------------------------POETRY------------------------------ //
 var order = 3; // number of letters in the n-gram
 var ngrams = {}; // JS object to store info on all possible outcomes for given n-gram
-var beginnings = [];
-var verseLength = 30;
-var result;
-var txt = $.ajax({
+var beginnings = []; // Array to store all possible beginnings
+var verseLength = 30; // Length of the verse
+var result; // variable to store results from both markov and riMarkov functions
+
+var txt = $.ajax({ // loading text from .txt file
   url: "assets/txt/comMan.txt",
   async: false
 }).responseText;
 
 var randomGram;
 var sentences;
-var words;
 
 $(document).ready(function() {
 // -----------------------IMAGES ON READY----------------------------- //
-// On document ready select random images and display them at random position and randmly scaled on the screen
+// On document ready select random images and display them at random position and randomly scaled on the screen
   for (let i = 0; i < imageQty; i ++) {
     addImage();
   }
 
+  // Make canvas resizable
   $( ".resizable" ).resizable({
-    alsoResize: "#mirror"
+    alsoResize: "#mirror" // Resize it from both sides to preserve symmetry
   });
 
+  // Make #bin div a droppable
   $('#bin').droppable({
     drop: function(event, ui) {
       $(this)
-      ui.draggable.remove();
+      ui.draggable.remove(); // Remove draggable when dragged on top of droppable
     }
   });
 
-  $( "#instructions" ).hide();
+  $( "#instructions" ).hide(); // Hide instructions by default
   $( "#instructionsHeader" ).click(function() {
-    $( "#instructions" ).toggle();
+    $( "#instructions" ).toggle(); // On mouse click toggle instructions on/off
   });
 
-// Possible transformations for when mouse is over an image and it has a class of "active"
+// Possible transformations for when mouse is over an image and the image has a class of "active"
   $(this).keypress(function(event) {
-    console.log(event.which);
+    //console.log(event.which);
 
     switch(event.which) {
 
@@ -114,7 +118,7 @@ $(document).ready(function() {
       scaleX *= -1;
       break;
 
-      case 105:
+      case 105: // Calls "addImage" function to add a new image
       addImage();
       break;
     }
@@ -122,11 +126,7 @@ $(document).ready(function() {
 //z = 122; x = 120;
   // ------------------------POETRY ON READY----------------------------- //
 
-  //var riTxt = new RiString(txt); // Split the string of text into individual words
   sentences = RiTa.splitSentences(txt); // Splitting the mass of text into individual sentences
-  //words = riTxt.words();
-  // //
-  // var randomWord = words[Math.floor(Math.random()*words.length)];
 
   // Creating a list of all n-grams existing in the reference text
   for (var j = 0; j < sentences.length; j++) {
@@ -144,7 +144,6 @@ $(document).ready(function() {
       }
       // Push values in the created/existing array
         ngrams[gram].push(outTxt.charAt(i + order));
-      // ngrams.push(gram);
     }
   }
 
@@ -168,43 +167,44 @@ $(document).ready(function() {
 })
 
 // -----------------------POETRY FUNCTIONS----------------------------- //
+// Function to create an output from n-grams
 function markov() {
-  var currentGram = beginnings[Math.floor(Math.random() * beginnings.length)]; // the n-gram that we start the poem with
+  var currentGram = beginnings[Math.floor(Math.random() * beginnings.length)]; // a random n-gram that we start the poem with
   result = currentGram;
 
   for (var i = 0; i < verseLength; i++) {
     var possibilities = ngrams[currentGram]; // for the current n-gram check the array of possibilities for the next char
     var next = possibilities[Math.floor(Math.random() * possibilities.length)]; // pick a random option from this array
-    result += next;
+    result += next; // add it to the current result
     currentGram = result.substring(result.length - order, result.length);
     //console.log(currentGram);
   }
-  pushResult();
-  console.log(result);
+  pushResult(); // call pushResult function to output result on the page
 }
 
+// Function to create an output from words
 function riMarkov() {
-  var riMarkovTxt = new RiMarkov(2);
-  riMarkovTxt.loadText(txt);
-  result = riMarkovTxt.generateSentences(1);
+  var riMarkovTxt = new RiMarkov(2); // number of words in order to be taken into account while calculating probabilities
+  riMarkovTxt.loadText(txt); // load original text into RiMarkov string
+  result = riMarkovTxt.generateSentences(1); // based on order number generate given number of sentences
 
-  pushResult();
-  console.log(result);
+  pushResult(); // call pushResult function to output result on the page
 }
 
+// Function to clear the last output result
 function clear() {
-  //while (result.firstChild) {
-    result.removeChild(result.lastChild);
-  //}
+    output.removeChild(output.lastChild);
 }
 
+// Function to create a p DOM element and display results on the page
 function pushResult() {
   var paragraph = document.createElement("P"); // Create a p element in the DOM
   paragraph.innerText = result;
-  document.getElementById("result").appendChild(paragraph); // Append p to the "poem" div
+  document.getElementById("output").appendChild(paragraph); // Append p to the "output" div
 }
 
 // -----------------------IMAGES FUNCTIONS----------------------------- //
+// Adding an array of images of random sizes at random positions
 function addImage() {
   let randomTop = Math.floor(Math.random() * 350);
   let randomLeft = Math.floor(Math.random() * 850);
@@ -212,12 +212,12 @@ function addImage() {
   $('<img src="assets/images/' + images[Math.floor(Math.random() * images.length)] + '" style="top:'+randomTop+'px; left:'+randomLeft+'px; width:'+randomWidth+'px; max-width: 100%;">').appendTo('#collage');
 
   $('img').addClass("images"); // adding a class to all <img> DOM elements
-  $('#pointer').removeClass("images");
+  $('#pointer').removeClass("images"); // removing "images" class from the elements that are not supposed to be draggable
 
   $('.images').draggable({
     stack: ".images",
     scroll: false
-  }); // making images draggable to move them around and to set currently dragged image to front
+  }); // making images draggable to move them around and to bring currently dragged image to front
 
   // When the mouse is over an image it becomes active and allows for transfomations
   $('.images').mouseover(function() {
